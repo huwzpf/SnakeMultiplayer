b2f5c7f09e11d32a88b5a0063de89564cdc16e16
MAJ0RRR <sebastian.lesniewski.edu@gmail.com>
2022-05-17 13:39:37 +0200

    Reduced package size

--- client/data_types.py
+++ client/data_types.py
@@ -49,13 +49,13 @@ class InitData:
 class PlayerState:
     def __init__(self, *args):
         arg = args[0]
-        self.ID = arg[0]
-        self.alive = arg[1]
-        self.draw = arg[2]
+        self.ID = int.from_bytes(arg[0],"big")
+        self.alive = int.from_bytes(arg[1],"big")
+        self.draw = int.from_bytes(arg[2],"big")
         self.dx = arg[3]
         self.dy = arg[4]
         self.size = arg[5]
-        self.bonus = arg[6]
+        self.bonus = int.from_bytes(arg[6],"big")
 
 
 class BonusState:
@@ -63,36 +63,36 @@ class BonusState:
         arg = args[0]
         self.x = arg[0]
         self.y = arg[1]
-        self.type = arg[2]
-        self.active = arg[3]
+        self.type = int.from_bytes(arg[2],"big")
+        self.active = int.from_bytes(arg[3],"big")
 
 
 class StateUpdate:
     def __init__(self, data, player_count):
-        self.running = struct.unpack("<i", data[-4:])[0]
+        self.running = int.from_bytes(struct.unpack("<c", data[-1:])[0],"big")
         self.players_data = []
         self.bonuses = []
         if self.running != 0:
             """
-            int id      4
-            int alive   4
-            int draw    4
-            double dx   8
-            double dy   8
-            double size 8
-            int bonus   4
+            char id      1
+            char alive   1
+            char draw    1
+            float dx     4
+            float dy     4
+            float size   4
+            char bonus   1
             ----------------+
-                        40 bytes -----------------------|                        
+                        16 bytes -----------------------|                        
                                                         V
             """
-            it_p = struct.iter_unpack("<iiidddi", data[:(40 * player_count)])
+            it_p = struct.iter_unpack("<cccfffc", data[:(16 * player_count)])
             """
-            int x
-            int y
-            int type
-            int active
+            unsigned short x
+            unsigned short y
+            char type
+            char active
             """
-            it_b = struct.iter_unpack("<iiii", data[(40 * player_count):-4])
+            it_b = struct.iter_unpack("<HHcc", data[(16 * player_count):-1])
             while True:
                 try:
                     self.players_data.append(PlayerState(list(next(it_p))))
